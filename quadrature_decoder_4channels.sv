096ef112bee484438e0774e67aea70dd760ead8e
letrend <simontrendel@gmail.com>
2022-04-18 16:52:28 +0200

    debounced

--- quadrature_decoder_4channels.sv
+++ quadrature_decoder_4channels.sv
@@ -1,6 +1,6 @@
 module Quadrature_Decoder_4Channels (
     input clk,
-    input reset,
+    input reset_n,
     input read,
     output unsigned [31:0] readdata,
     input write,
@@ -20,64 +20,57 @@ module Quadrature_Decoder_4Channels (
       dir <= writedata[0];
     end
   end
+  
+  reg [7:0] counter [3:0];
+  reg [3:0] channel_debounced;
+  
+  genvar j;
+  generate for (j = 0; j < 4; j = j+1)
+  begin:  debounce_counter_loop
+    always @ (posedge clk) begin
+		if(channel[j]!=channel_debounced[j])begin
+			counter[j] <= counter[j]+1;
+			if(counter[j]==0)begin
+				channel_debounced[j] <= channel[j];
+			end
+		end else begin
+			counter[j] <= 1;
+		end
+	 end
+ end
+  endgenerate
 
   assign readdata = dir?(pos-offset):(offset-pos);
 	
 	integer i;
-	reg [3:0] channel_prev;
 	
-	genvar j;
+	reg [3:0] channel_prev;
 	wire [3:0] rising_edge;
 	wire [3:0] falling_edge;
 	generate
 		for(j=0;j<4;j=j+1)begin: rising_falling
-			assign rising_edge[j] = channel[j] && !channel_prev[j];
-			assign falling_edge[j] = !channel[j] && channel_prev[j];
+			assign rising_edge[j] = channel_debounced[j] && !channel_prev[j];
+			assign falling_edge[j] = !channel_debounced[j] && channel_prev[j];
 		end
 	
 	endgenerate
 	
 	always @( posedge clk ) begin
 		for(i=0;i<4;i=i+1)begin
-			channel_prev[i] <= channel[i];
+			channel_prev[i] <= channel_debounced[i];
 		end
-//		if(channel[0] && falling_edge[1])begin
-//				pos <= pos+1;
-//		end
-//		if(!channel[1] && falling_edge[0])begin
-//				pos <= pos+1;
-//		end
-//		if(!channel[0] && rising_edge[1])begin
-//				pos <= pos+1;
-//		end
-//		if(channel[1] && rising_edge[0])begin
-//				pos <= pos+1;
-//		end
-//		
-//		if(channel[1] && falling_edge[0])begin
-//				pos <= pos-1;
-//		end
-//		if(!channel[0] && falling_edge[1])begin
-//				pos <= pos-1;
-//		end
-//		if(!channel[1] && rising_edge[0])begin
-//				pos <= pos-1;
-//		end
-//		if(channel[0] && rising_edge[1])begin
-//				pos <= pos-1;
-//		end
 		
 		if(rising_edge[0])begin
-			pos <= pos+(channel[1]?1:-1);
+			pos <= pos+(channel_debounced[1]?1:-1);
 		end
 		if(rising_edge[1])begin
-			pos <= pos+(channel[0]?-1:1);
+			pos <= pos+(channel_debounced[0]?-1:1);
 		end
 		if(falling_edge[0])begin
-			pos <= pos+(channel[1]?-1:1);
+			pos <= pos+(channel_debounced[1]?-1:1);
 		end
 		if(falling_edge[1])begin
-			pos <= pos+(channel[0]?1:-1);
+			pos <= pos+(channel_debounced[0]?1:-1);
 		end
 	end
 
